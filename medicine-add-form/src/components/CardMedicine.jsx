6c669df049b3cb94cffc35833c2a2248a027372b
Denison Kolling <denisonkolling@gmail.com>
2023-08-07 09:53:01 -0300

    fix: local storage setItem Favorito

--- medicine-add-form/src/components/CardMedicine.jsx
+++ medicine-add-form/src/components/CardMedicine.jsx
@@ -1,26 +1,30 @@
-
 import React, { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { MedicineContext } from '../context/MedicineContext';
 
 const CardMedicine = ({ medicine }) => {
-
-	const {FavoriteMedicine} = useContext(MedicineContext)
+	const { FavoriteMedicine } = useContext(MedicineContext);
 
 	return (
 		<div className="card col-2 m-4">
 			<div className="card-body">
-			<button className={ medicine.favorite ? 'btn text-danger' : 'btn text-muted'} onClick={() => FavoriteMedicine(medicine.id)}>
-			<FontAwesomeIcon icon={faHeart} /></button>
-				<img
-					className="card-img-top"
-					src="https://araujo.vteximg.com.br/arquivos/ids/4204882-1000-1000/07891045042741.jpg?v=638119987241930000"
-					alt="Card image cap"></img>
+				<button
+					className={medicine.favorite ? 'btn text-danger' : 'btn text-muted'}
+					onClick={() => FavoriteMedicine(medicine.id)}>
+					<FontAwesomeIcon icon={faHeart} />
+				</button>
+				<div>
+					<img
+						className="card-img-top"
+						src="https://araujo.vteximg.com.br/arquivos/ids/4204882-1000-1000/07891045042741.jpg?v=638119987241930000"
+						alt="Card image cap"></img>
+				</div>
+				<div className='my-2'>
 				<h5 className="card-title text-center">{medicine.name}</h5>
+				</div>
 				<p className="text-center">{medicine.laboratory}</p>
-				<p className="text-center">$ {medicine.price}</p>
-				<p className="text-center">{medicine.favorite.toString()}</p>
+				<p className="text-center align-text-bottom">$ {medicine.price}</p>
 			</div>
 		</div>
 	);
--- medicine-add-form/src/context/MedicineContext.jsx
+++ medicine-add-form/src/context/MedicineContext.jsx
@@ -3,8 +3,9 @@ import { createContext, useState } from 'react';
 export const MedicineContext = createContext();
 
 export const MedicineContextProvider = ({ children }) => {
-
-	const [listMedicine, setListMedicine] = useState(JSON.parse(localStorage.getItem('listMedicine')) || []);
+	const [listMedicine, setListMedicine] = useState(
+		JSON.parse(localStorage.getItem('listMedicine')) || []
+	);
 
 	const AddMedicine = (name, laboratory, price) => {
 		if (name.length == '' || laboratory.length == '' || price == 0) {
@@ -21,25 +22,27 @@ export const MedicineContextProvider = ({ children }) => {
 		};
 
 		const newListMedicine = [...listMedicine, newMedicine];
-    localStorage.setItem('listMedicine', JSON.stringify(newListMedicine))
+		localStorage.setItem('listMedicine', JSON.stringify(newListMedicine));
 		setListMedicine(newListMedicine);
 		console.log(newListMedicine);
 		alert('Medicine added successfuly!');
 	};
 
-  const FavoriteMedicine = (id) => {
-    const list = listMedicine.map((item) => {
-      if (item.id == id) {
-        item.favorite = !item.favorite;
-      }
+	const FavoriteMedicine = (id) => {
+		const list = listMedicine.map((item) => {
+			if (item.id == id) {
+				item.favorite = !item.favorite;
+			}
 
-      return item;
-    });
-  
-    setListMedicine(list);
-  };
-  
-  
+			const newListMedicine = [...listMedicine, item];
+			localStorage.setItem('listMedicine', JSON.stringify(newListMedicine));
+			setListMedicine(newListMedicine);
+			return item;
+		});
+
+		setListMedicine(list);
+	};
+
 	return (
 		<MedicineContext.Provider
 			value={{ listMedicine, AddMedicine, FavoriteMedicine }}>
@@ -47,4 +50,3 @@ export const MedicineContextProvider = ({ children }) => {
 		</MedicineContext.Provider>
 	);
 };
-
